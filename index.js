e0ba0dc464e18fc8e8be570b7b4d8d7c50d881e1
yesiamjc <chakrabortyjoy638@gmail.com>
2025-01-24 00:41:26 +0530

    portfolio

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerBtn = document.querySelector('.hamburgerBtn');
+    const hamburgerMenu = document.querySelector('.hamburgerMenu');
+    const closeBtn = document.querySelector('.closeBtn');
+
+    hamburgerBtn.addEventListener('click', function() {
+        hamburgerMenu.style.display = 'flex';
+    });
+
+    closeBtn.addEventListener('click', function() {
+        hamburgerMenu.style.display = 'none';
+    });
+});
